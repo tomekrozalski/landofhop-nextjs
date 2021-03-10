468f8b58a013bcbb5ceb48fb0ffcce189b3a53a8
Tomek Rozalski <alium@daucusetanser.pl>
2021-03-10 22:05:26 +0100

    [Config] Add AdSense for test

--- elements/HeadTitle.tsx
+++ elements/HeadTitle.tsx
@@ -12,6 +12,11 @@ const HeadTitle: React.FC<Props> = ({ title, values = {} }) => {
   return (
     <Head>
       <title>{formatMessage({ id: `titles.${title}` }, values)}</title>
+      <script
+        data-ad-client="ca-pub-3743392577349195"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      ></script>
     </Head>
   );
 };
