1650d9804d128d0b3ed2e37aea4a7e1e30c5b1e2
Tomek Rozalski <alium@daucusetanser.pl>
2021-04-07 08:18:37 +0200

    [Learning] Test with cookie in _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,37 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 
-export default class MyDocument extends Document {
+type Props = {
+  cookies: string[];
+};
+export default class MyDocument extends Document<Props> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    if (ctx.req && ctx.req.headers.cookie) {
+      return {
+        ...initialProps,
+        cookies: ctx.req.headers.cookie.split(`;`).map(value => value.trim()),
+      };
+    }
+
+    return { ...initialProps, cookies: [] };
+  }
+
   render() {
+    const isVisited = !!this.props.cookies.find(
+      cookie => cookie === 'isVisited=true',
+    );
+
     return (
       <Html>
         <Head />
-        <body>
+        <body className={isVisited ? `non-active` : `active-cookie-bar`}>
           <Main />
           <NextScript />
           <div id="modal-root" />
